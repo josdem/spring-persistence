9c01f584ab9909ce7b786e4f27d82b89d4df5866
neodevelop <neodevelop@gmail.com>
2013-12-30 17:27:10 -0600

    creando el temario

--- pages/toc.groovy
+++ pages/toc.groovy
@@ -5,16 +5,46 @@
   "presentacion.html#4"(title: "Herramientas para entrenamiento")
 }
 
-"essentials.html"(title:"Conceptos esenciales de acceso a datos con Spring")
+"essentials.html"(title:"Conceptos esenciales de acceso a datos con Spring"){
+  "essentials.html"(title:"El uso de JDBC")
+  "essentials.html"(title:"Jerarquía de excepciones")
+  "essentials.html"(title:"Acceso a datos por Templates")
+  "essentials.html"(title:"Soporte a DAO")
+  "essentials.html"(title:"¿ActiveRecord?")
+}
 
-"configuration.html"(title:"Configuración básica para persistencia")
+"configuration.html"(title:"Configuración básica para persistencia"){
+  "configuration.html"(title:"La base de datos y el namespace")
+  "configuration.html"(title:"Control de las conexiones(*DataSource)")
+  "configuration.html"(title:"El DAO genérico")
+  "configuration.html"(title:"Pruebas de integración")
+}
 
-"jdbctemplate.html"(title:"Uso del JdbcTemplate")
+"jdbctemplate.html"(title:"Uso del JdbcTemplate"){
+  "jdbctemplate.html"(title:"Inicio rápido con el SimpleJdbcTemplate")
+  "jdbctemplate.html"(title:"Modelado de las operaciones JDBC como objetos Java")
+  "jdbctemplate.html"(title:"El JdbcTemplate y los RowMappers")
+  "jdbctemplate.html"(title:"Creando DAO's con JdbcDaoSupport")
+}
 
-"hibernate.html"(title:"Hibernate y Spring")
+"hibernate.html"(title:"Hibernate y Spring"){
+  "hibernate.html"(title:"Consideraciones para el uso de ORM's")
+  "hibernate.html"(title:"El SessionFactory")
+  "hibernate.html"(title:"Implementación de DAO's con Hibernate")
+}
 
-"transactions.html"(title:"Manejo de transacciones")
+"transactions.html"(title:"Manejo de transacciones"){
+  "transactions.html"(title:"Conceptos esenci")
+  "transactions.html"(title:"Manejadores de transacciones")
+  "transactions.html"(title:"Programando transacciones")
+  "transactions.html"(title:"Transacciones declarativas")
+  "transactions.html"(title:"Transacciones con anotaciones")
+}
 
-"liquibase.html"(title:"Versionado de base de datos")
+"liquibase.html"(title:"Refactor de base de datos"){
+  "liquibase.html"(title:"El archivo changelog")
+  "liquibase.html"(title:"Conjunto de cambios: El changeset")
+  "liquibase.html"(title:"Integración con Spring")
+}
 
 "agradecimiento.html"(title: "Agradecimiento")
